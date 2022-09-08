0b3ceaca0a4057fafb33849b9fbd2a448615c982
Donia Chaiehloudj <donia.cld@gmail.com>
2022-09-08 18:45:00 +0200

    Add exercice 01: Print any type

--- 01-type-parameters/exercice-01/main.go
+++ 01-type-parameters/exercice-01/main.go
@@ -1,1 +1,36 @@
-package exercice_01
+package main
+
+import "fmt"
+
+// version without generics
+func printString(s string) { fmt.Println(s) }
+
+func printInt(i int) { fmt.Println(i) }
+
+func printBool(b bool) { fmt.Println(b) }
+
+// version with generics
+// print the input regarding its type
+func print[T interface{}](input T) { // we could use any here but, it was not yet introduced in the workshop
+	fmt.Println(input)
+}
+
+func main() {
+	// version without generics
+	printString("hello")
+	printInt(5)
+	printBool(false)
+	// Output:
+	// hello
+	// 5
+	// false
+
+	// version with generics
+	print("hello")
+	print(5)
+	print(false)
+	// Output:
+	// hello
+	// 5
+	//false
+}
